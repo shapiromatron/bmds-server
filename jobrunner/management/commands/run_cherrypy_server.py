aae0e394f08997beee98b8ed0f1ee8d458c467c7
Andy Shapiro <shapiromatron@gmail.com>
2016-11-08 12:24:23 -0500

    use cherrypy webserver, more battle-tested

---
+++ jobrunner/management/commands/run_cherrypy_server.py
@@ -0,0 +1,29 @@
+"""
+Adapted from: https://lincolnloop.com/blog/2008/mar/25/serving-django-cherrypy/.
+Updated and simplified.
+"""
+import os
+from django.core.management.base import BaseCommand
+
+from bmds_server.wsgi import application
+from cherrypy import wsgiserver
+
+
+HELP_TEXT = """Serve application using CherryPy WSGI server."""
+
+
+class Command(BaseCommand):
+
+    help = HELP_TEXT
+
+    def handle(self, *args, **options):
+
+        # Added to environment by IIS
+        port = int(os.environ.get('HTTP_PLATFORM_PORT', 8000))
+
+        server = wsgiserver.CherryPyWSGIServer(
+            ('0.0.0.0', port),
+            application
+        )
+
+        server.start()
--- simple_serve.py
+++
@@ -1,8 +0,0 @@
-import os
-from wsgiref.simple_server import make_server
-
-from bmds_server.wsgi import application
-
-port = int(os.environ.get('HTTP_PLATFORM_PORT', 5555))
-httpd = make_server('', port, application)
-httpd.serve_forever()

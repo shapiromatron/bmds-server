ffdf5cfe0a9285f0dacce1ca0131ed82048cffae
Andy Shapiro <shapiromatron@gmail.com>
2016-11-17 15:04:19 -0500

    disable throttling in dev

--- bmds_server/settings/dev.py
+++ bmds_server/settings/dev.py
@@ -5,3 +5,9 @@ SECRET_KEY = '4+7p%-l!_d_7lkn6y16jfpn-i=ul(9=$e@-+%s^-st3bcpx2^&'
 
 CELERY_ALWAYS_EAGER = True
 CELERY_EAGER_PROPAGATES_EXCEPTIONS = True
+
+# disable throttling
+REST_FRAMEWORK = {
+    'DEFAULT_THROTTLE_CLASSES': (),
+    'DEFAULT_THROTTLE_RATES': {}
+}

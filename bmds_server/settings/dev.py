21fcfee32070644edf10dc84a166f83e98441f2d
Andy Shapiro <shapiromatron@gmail.com>
2016-11-22 10:45:10 -0500

    allow for jupyter notebooks in development

--- bmds_server/settings/dev.py
+++ bmds_server/settings/dev.py
@@ -1,5 +1,9 @@
 from bmds_server.settings.base import *  # noqa
 
+INSTALLED_APPS += (
+    'django_extensions',
+)
+
 DEBUG = True
 SECRET_KEY = '4+7p%-l!_d_7lkn6y16jfpn-i=ul(9=$e@-+%s^-st3bcpx2^&'
 

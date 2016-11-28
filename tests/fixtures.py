84a4133e296e0f37413ed6f78e171f758afd319b
Andy Shapiro <shapiromatron@gmail.com>
2016-11-28 14:36:06 -0500

    move pytest fixtures to separate file

---
+++ tests/fixtures.py
@@ -0,0 +1,34 @@
+import pytest
+
+
+@pytest.fixture
+def complete_continuous():
+    return {
+        'bmds_version': 'BMDS2601',
+        'dataset_type': 'C',
+        'datasets': [
+            {
+                'id': 123,
+                'doses': [0, 10, 50, 150, 400],
+                'ns': [111, 142, 143, 93, 42],
+                'responses': [2.112, 0, 1.956, 1.587, 1.254],
+                'stdevs': [0.235, 0, 0.231, 0.263, 0.159]
+            }
+        ]
+    }
+
+
+@pytest.fixture
+def complete_dichotomous():
+    return {
+        'bmds_version': 'BMDS2601',
+        'dataset_type': 'D',
+        'datasets': [
+            {
+                'id': 123,
+                'doses': [0, 1.96, 5.69, 29.75],
+                'ns': [75, 49, 50, 49],
+                'incidences': [5, 0, 3, 14]
+            },
+        ]
+    }

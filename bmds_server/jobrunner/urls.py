a0ed1a500be4cc38f34232cce649a924ac82ec91
Andy Shapiro <shapiromatron@gmail.com>
2020-03-18 16:58:16 -0400

    fix isort

--- bmds_server/jobrunner/urls.py
+++ bmds_server/jobrunner/urls.py
@@ -1,5 +1,5 @@
-from django.conf.urls import include
 from django.conf import settings
+from django.conf.urls import include
 from django.contrib import admin
 from django.urls import path
 from rest_framework.routers import DefaultRouter

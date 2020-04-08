2c22f38f90064a4db2c3c6ec178d853f04169f33
Andy Shapiro <shapiromatron@gmail.com>
2020-04-08 11:03:03 -0400

    integrate back into app

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,22 @@
-
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
-import "./index.css"
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
 
-import {Provider} from 'mobx-react';
-import DataStore from './stores/DataStore'
+import {Provider} from "mobx-react";
+import DataStore from "./stores/DataStore";
 
-const Root=(
+const Root = (
     <Provider DataStore={DataStore}>
-    <App/>
+        <App />
     </Provider>
-)
- 
-ReactDOM.render(Root, document.getElementById("root"));
+);
+
+const appStartup = function(el) {
+    ReactDOM.render(Root, el);
+};
+
+window.app = {
+    appStartup,
+};
---
+++ frontend/webpack.base.js
@@ -0,0 +1,55 @@
+const path = require("path"),
+    BundleTracker = require("webpack-bundle-tracker");
+
+module.exports = {
+    context: __dirname,
+
+    entry: {
+        main: ["./src/index"],
+    },
+
+    mode: "development",
+
+    module: {
+        rules: [
+            {
+                exclude: /node_modules/,
+                test: /\.js$/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        plugins: [
+                            "@babel/plugin-syntax-dynamic-import",
+                            ["@babel/plugin-proposal-class-properties", {loose: false}],
+                        ],
+                    },
+                },
+            },
+            {
+                test: /\.css$/,
+                loader: "style-loader!css-loader",
+            },
+        ],
+    },
+
+    output: {
+        filename: "[name].[hash].js",
+        chunkFilename: "[name].[hash].js",
+        path: path.resolve("../bmds_server/static/bundles"),
+        publicPath: "/static/bundles/",
+    },
+
+    plugins: [
+        new BundleTracker({
+            filename: "../webpack-stats.json",
+        }),
+    ],
+
+    resolve: {
+        alias: {
+            bmd: path.join(__dirname, "src"),
+        },
+        modules: [path.join(__dirname, "src"), "node_modules"],
+        extensions: [".js", ".css"],
+    },
+};

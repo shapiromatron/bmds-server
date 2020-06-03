d9cef3c130f492085cf43daabeae8f327f6ce3d2
Bishwo Bhandari <bhanbish@isu.edu>
2020-06-03 09:19:57 -0400

    extra <form> removed

--- frontend/src/components/Main/OptionsFormList.js
+++ frontend/src/components/Main/OptionsFormList.js
@@ -30,48 +30,44 @@ class OptionsFormList extends Component {
                 <div className="row" style={{marginTop: 20}}>
                     <div className="col">
                         <div>
-                            <form>
-                                <table className="table table-bordered">
-                                    <thead>
-                                        <tr>
-                                            <th>Option Set #</th>
-                                            {dataset_type === "C" ? <th>BMR Type</th> : null}
-                                            {dataset_type === "D" ? <th>Risk Type</th> : null}
-                                            <th>BMRF</th>
-                                            {dataset_type === "C" ? (
-                                                <th>Tail Probability</th>
-                                            ) : null}
-                                            <th>Confidence Level</th>
-                                            {dataset_type === "C" ? <th>Distribution</th> : null}
-                                            {dataset_type === "C" ? <th>Variance</th> : null}
-                                            {dataset_type === "C" ? (
-                                                <th>Polynomial Restriction</th>
-                                            ) : null}
-                                            <th>Background</th>
-                                            <th></th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        {options.map((item, id) => (
-                                            <OptionsForm
-                                                className="form-control"
-                                                key={id}
-                                                item={item}
-                                                dataset_type={dataset_type}
-                                                idx={id}
-                                                onchange={this.onChange}
-                                                delete={this.deleteOption.bind(this)}
-                                            />
-                                        ))}
-                                    </tbody>
-                                </table>
-                                <button
-                                    type="button"
-                                    className="btn btn-primary "
-                                    onClick={() => store.createOptions()}>
-                                    add option set
-                                </button>
-                            </form>
+                            <table className="table table-bordered">
+                                <thead>
+                                    <tr>
+                                        <th>Option Set #</th>
+                                        {dataset_type === "C" ? <th>BMR Type</th> : null}
+                                        {dataset_type === "D" ? <th>Risk Type</th> : null}
+                                        <th>BMRF</th>
+                                        {dataset_type === "C" ? <th>Tail Probability</th> : null}
+                                        <th>Confidence Level</th>
+                                        {dataset_type === "C" ? <th>Distribution</th> : null}
+                                        {dataset_type === "C" ? <th>Variance</th> : null}
+                                        {dataset_type === "C" ? (
+                                            <th>Polynomial Restriction</th>
+                                        ) : null}
+                                        <th>Background</th>
+                                        <th></th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {options.map((item, id) => (
+                                        <OptionsForm
+                                            className="form-control"
+                                            key={id}
+                                            item={item}
+                                            dataset_type={dataset_type}
+                                            idx={id}
+                                            onchange={this.onChange}
+                                            delete={this.deleteOption.bind(this)}
+                                        />
+                                    ))}
+                                </tbody>
+                            </table>
+                            <button
+                                type="button"
+                                className="btn btn-primary "
+                                onClick={() => store.createOptions()}>
+                                add option set
+                            </button>
                         </div>
                     </div>
                 </div>

4a29f2c6c1de52ea67b29a239574725a411890ed
Andy Shapiro <shapiromatron@gmail.com>
2020-06-30 14:17:53 -0400

    lint/format

--- frontend/src/components/Inputs/FloatInput.js
+++ frontend/src/components/Inputs/FloatInput.js
@@ -1,7 +1,6 @@
 import React, {Component} from "react";
 import PropTypes from "prop-types";
 
-
 const reFloat = new window.RegExp(/^-?[\d]+\.?[\d]*[eE]?-?\d*$/);
 
 class FloatInput extends Component {
@@ -12,7 +11,7 @@ class FloatInput extends Component {
         };
     }
     render() {
-        const { id, name, onChange, minimum, maximum} = this.props;
+        const {id, name, onChange, minimum, maximum} = this.props;
         return (
             <input
                 type="text"
@@ -24,20 +23,20 @@ class FloatInput extends Component {
                 onChange={e => {
                     // edge case; handle if user enters "." which defaults to 0
                     let strValue = e.target.value;
-                    if (strValue.slice(0, 1) === '.'){
+                    if (strValue.slice(0, 1) === ".") {
                         strValue = "0" + strValue;
                     }
                     // check if float, and value is NaN, and min/max if those exists
                     const val = parseFloat(strValue);
                     if (
-                        (reFloat.test(strValue) === false) ||
-                        (window.isNaN(val)) ||
+                        reFloat.test(strValue) === false ||
+                        window.isNaN(val) ||
                         (minimum && val < minimum) ||
                         (maximum && val > maximum)
-                    ){
-                        return
+                    ) {
+                        return;
                     }
-                    this.setState({ strValue, });
+                    this.setState({strValue});
                 }}
             />
         );
@@ -50,7 +49,7 @@ FloatInput.propTypes = {
     value: PropTypes.number.isRequired,
     onChange: PropTypes.func.isRequired,
     minimum: PropTypes.number,
-    maximum: PropTypes.number
+    maximum: PropTypes.number,
 };
 
 export default FloatInput;

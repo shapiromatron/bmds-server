9aa431c36fd931ccdb12ee4e699d55d9c36ff4a6
Andy Shapiro <shapiromatron@gmail.com>
2020-07-01 12:23:58 -0400

    fix settings w/ celery migration

--- bmds_server/main/settings/dev.py
+++ bmds_server/main/settings/dev.py
@@ -1,5 +1,4 @@
 # flake8: noqa
-
 from .base import *
 
 INSTALLED_APPS += ("django_extensions",)
@@ -11,8 +10,8 @@ SECRET_KEY = "4+7p%-l!_d_7lkn6y16jfpn-i=ul(9=$e@-+%s^-st3bcpx2^&"
 LOGGING["loggers"][""]["handlers"] = ["console"]
 LOGGING["loggers"][""]["level"] = "INFO"
 
-CELERY_ALWAYS_EAGER = True
-CELERY_EAGER_PROPAGATES_EXCEPTIONS = True
+CELERY_TASK_ALWAYS_EAGER = True
+CELERY_TASK_EAGER_PROPAGATES = True
 
 # disable throttling
 REST_FRAMEWORK = {"DEFAULT_THROTTLE_CLASSES": (), "DEFAULT_THROTTLE_RATES": {}}

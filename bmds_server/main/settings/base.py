f559a2708b41e30460dc8b5eacfb13fb62a64ea8
Andy Shapiro <shapiromatron@gmail.com>
2020-10-27 16:40:34 -0400

    remove python-decouple

--- bmds_server/main/settings/base.py
+++ bmds_server/main/settings/base.py
@@ -3,15 +3,19 @@
 import os
 from pathlib import Path
 
-from decouple import config
-
 from ..constants import SkinStyle
 
 PROJECT_NAME = "bmds-server"
 BASE_DIR = Path(__file__).parents[2].resolve()
 ROOT_DIR = Path(__file__).parents[3].resolve()
+
+# data paths
 PUBLIC_DATA_ROOT = Path(os.environ.get("PUBLIC_DATA_ROOT", ROOT_DIR / "public"))
-LOGS_PATH = Path(os.environ.get("LOGS_PATH", ROOT_DIR))
+LOGS_PATH = Path(os.environ.get("LOGS_PATH", ROOT_DIR / "logs"))
+
+# make sure these paths exist
+PUBLIC_DATA_ROOT.mkdir(exist_ok=True, parents=False)
+LOGS_PATH.mkdir(exist_ok=True, parents=False)
 
 SKIN = SkinStyle.Base
 
@@ -60,6 +64,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = "bmds_server.main.wsgi.application"
+SECRET_KEY = "io^^q^q1))7*r0u@6i+6kx&ek!yxyf6^5vix_6io6k4kdn@@5t"
 
 DATABASES = {
     "default": {"ENGINE": "django.db.backends.sqlite3", "NAME": str(ROOT_DIR / "db.sqlite3"),}
@@ -68,7 +73,7 @@ DATABASES = {
 LOGIN_URL = "admin:login"
 
 # add randomness to url prefix to prevent easy access
-ADMIN_URL_PREFIX = os.getenv("ADMIN_URL_PREFIX", "f09ea0b8-c3d5-4ff9-86c4-27f00e8f643d")
+ADMIN_URL_PREFIX = "f09ea0b8-c3d5-4ff9-86c4-27f00e8f643d"
 
 AUTH_PASSWORD_VALIDATORS = [
     {"NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"},
@@ -86,7 +91,6 @@ USE_TZ = True
 EMAIL_SUBJECT_PREFIX = f"[{PROJECT_NAME}] "
 DEFAULT_FROM_EMAIL = f"webmaster@{PROJECT_NAME}.com"
 
-HTTP_PLATFORM_PORT = config("HTTP_PLATFORM_PORT", default=80, cast=int)
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [str(BASE_DIR / "static")]
 
--- bmds_server/main/wsgi.py
+++ bmds_server/main/wsgi.py
@@ -1,11 +1,10 @@
 import os
 
-from decouple import config
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault(
     "DJANGO_SETTINGS_MODULE",
-    config("DJANGO_SETTINGS_MODULE", default="bmds_server.main.settings.dev"),
+    os.environ.get("DJANGO_SETTINGS_MODULE", "bmds_server.main.settings.dev"),
 )
 
 application = get_wsgi_application()

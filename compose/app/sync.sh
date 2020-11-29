ae884c7cbea7df67a0db68e180f05d67f68915b9
Andy Shapiro <shapiromatron@gmail.com>
2020-11-29 17:28:46 -0500

    adding notes to sync commands for logging

--- compose/app/sync.sh
+++ compose/app/sync.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
-# quit on failure
+
 set -e
 
+echo "clearing cache..."
 manage.py clear_cache
+
+echo "migrating db..."
 manage.py migrate --noinput
+
+echo "collecting static files..."
 manage.py collectstatic --noinput
+
+echo "sync complete!"

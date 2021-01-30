4abae1a9d7f20ac52f19726a4fe619dd4a577808
Andy Shapiro <shapiromatron@gmail.com>
2021-01-30 15:22:21 -0500

    auto update document title

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,28 @@
 import React, {Component} from "react";
 import {HashRouter} from "react-router-dom";
-import Navigation from "./components/Navigation";
 import {inject, observer} from "mobx-react";
 import PropTypes from "prop-types";
+import {autorun} from "mobx";
+
+import Navigation from "./components/Navigation";
 
 @inject("mainStore")
 @observer
 class App extends Component {
-    render() {
+    componentDidMount() {
         const config = JSON.parse(document.getElementById("config").textContent);
         this.props.mainStore.setConfig(config);
         this.props.mainStore.fetchSavedAnalysis();
+
+        // update HTML document title
+        autorun(() => {
+            const {analysis_name, canEdit} = this.props.mainStore,
+                verb = canEdit ? "Update Analysis" : "Analysis",
+                name = analysis_name ? analysis_name : "";
+            document.title = ["BMDS Online", verb, name].join(" | ");
+        });
+    }
+    render() {
         return (
             <div>
                 {this.props.mainStore.isUpdateComplete ? (

e6e9090a5eca9f0e5afeb159bb9033b889ca447f
Andy Shapiro <shapiromatron@gmail.com>
2021-02-25 18:26:27 -0500

    fix tests; auto-handle line feeds

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,7 +8,7 @@ from django.core.management import call_command
 @pytest.fixture(scope="session", autouse=True)
 def test_db(django_db_setup, django_db_blocker):
     with django_db_blocker.unblock():
-        call_command("load_test_db", interactive=False)
+        call_command("load_test_db")
 
 
 @pytest.fixture(scope="session")

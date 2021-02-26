b3d958f9b8ad414760113854c1d2eac9855f2c58
Andy Shapiro <shapiromatron@gmail.com>
2021-02-26 14:15:25 -0500

    add --ifempty for loading test fixture for test deployments to potentially preserve state

--- bmds_server/jobrunner/management/commands/load_test_db.py
+++ bmds_server/jobrunner/management/commands/load_test_db.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.contrib.auth import get_user_model
 from django.core.management import call_command
 from django.core.management.base import BaseCommand, CommandError
 
@@ -8,11 +9,26 @@ HELP_TEXT = """Load the test database from a fixture."""
 class Command(BaseCommand):
     help = HELP_TEXT
 
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "--ifempty",
+            action="store_true",
+            dest="ifempty",
+            help="Only flush/load if database is empty",
+        )
+
     def handle(self, *args, **options):
 
         if "test" not in settings.DATABASES["default"]["NAME"]:
             raise CommandError("Must be using a test database to execute.")
 
         call_command("migrate", verbosity=0)
-        call_command("flush", verbosity=0, interactive=False)
-        call_command("loaddata", str(settings.TEST_DB_FIXTURE), verbosity=1)
+
+        if options["ifempty"] and get_user_model().objects.count() > 0:
+            message = "Migrations complete; fixtures not loaded (db not empty)"
+        else:
+            call_command("flush", verbosity=0, interactive=False)
+            call_command("loaddata", str(settings.TEST_DB_FIXTURE), verbosity=1)
+            message = "Migrations complete; fixture loaded"
+
+        self.stdout.write(self.style.SUCCESS(message))

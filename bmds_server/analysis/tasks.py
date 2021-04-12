17efc41ed004f02228130e04856363183506104e
Andy Shapiro <shapiromatron@gmail.com>
2021-04-12 15:41:31 -0400

    update to py3.9

--- bmds_server/analysis/tasks.py
+++ bmds_server/analysis/tasks.py
@@ -1,4 +1,4 @@
-from celery import task
+from celery import shared_task
 from celery.utils.log import get_task_logger
 from django.apps import apps
 
@@ -7,7 +7,7 @@ from .cache import DocxReportCache, ExcelReportCache
 logger = get_task_logger(__name__)
 
 
-@task()
+@shared_task()
 def try_execute(id_: str):
     analysis = apps.get_model("analysis", "Analysis").objects.get(id=id_)
     logger.info(f"starting execution: {analysis}")
@@ -15,13 +15,13 @@ def try_execute(id_: str):
     logger.info(f"finished execution: {analysis}")
 
 
-@task()
+@shared_task()
 def delete_old_analyses():
     logger.info("Deleting old analyses")
     apps.get_model("analysis", "Analysis").delete_old_analyses()
 
 
-@task()
+@shared_task()
 def generate_report(id_: str):
     logger.info(f"starting report generation: {id_}")
     analysis = apps.get_model("analysis", "Analysis").objects.get(id=id_)
@@ -29,7 +29,7 @@ def generate_report(id_: str):
     logger.info(f"finishing report generation: {id_}")
 
 
-@task()
+@shared_task()
 def generate_excel(id_: str):
     logger.info(f"starting excel generation: {id_}")
     analysis = apps.get_model("analysis", "Analysis").objects.get(id=id_)

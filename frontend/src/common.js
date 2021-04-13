9f499990e8a7e9365b9f00aa0eec655e5dd88da5
bishwobhandari <bhanbish@isu.edu>
2021-04-13 11:45:00 -0500

    Update common.js

--- frontend/src/common.js
+++ frontend/src/common.js
@@ -40,26 +40,4 @@ export const simulateClick = function(el) {
     },
     getLabel = function(value, mapping) {
         return _.find(mapping, d => d.value === value).label;
-    },
-    getPriorWeightValue = function(models, model) {
-        let prior_weight = 0;
-        if (mc.BAYESIAN_MODEL_AVERAGE in models) {
-            let obj = models[mc.BAYESIAN_MODEL_AVERAGE].find(obj => obj.model === model);
-
-            if (obj != undefined) {
-                prior_weight = obj.prior_weight;
-            }
-        }
-        return prior_weight;
-    },
-    isChecked = function(models, type, model) {
-        let checked = false;
-        if (type in models) {
-            if (type === mc.BAYESIAN_MODEL_AVERAGE) {
-                checked = models[type].findIndex(obj => obj.model === model) > -1;
-            } else {
-                checked = models[type].indexOf(model) > -1;
-            }
-        }
-        return checked;
     };

bb5e4b52985b22f46b53dede20346f27fc582d8e
bishwobhandari <bhanbish@isu.edu>
2021-05-07 16:15:15 -0500

    update production javascript library versions (#112)
    * updated java script library version on package.json
    
    * remove a few dependencies and sync packages and static
    
    * fix reset button
    
    Co-authored-by: Andy Shapiro <shapiromatron@gmail.com>

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import "../node_modules/font-awesome/css/font-awesome.min.css";
 
 import {Provider} from "mobx-react";
 import rootStore from "./stores/RootStore";

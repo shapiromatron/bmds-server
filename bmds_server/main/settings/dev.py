71e3574998421dee00a4a35d7c53e989b1ab2447
Andy Shapiro <shapiromatron@gmail.com>
2021-05-12 15:45:12 -0400

    package version updates

--- bmds_server/main/settings/dev.py
+++ bmds_server/main/settings/dev.py
@@ -1,10 +1,7 @@
 # flake8: noqa
 from .base import *
 
-INSTALLED_APPS += (
-    "django_extensions",
-    "debug_toolbar",
-)
+INSTALLED_APPS += ("debug_toolbar",)
 MIDDLEWARE += ("debug_toolbar.middleware.DebugToolbarMiddleware",)
 INTERNAL_IPS = ("127.0.0.1",)
 

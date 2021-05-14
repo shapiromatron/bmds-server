719beba6053b8a4710c77dfa3679c11ed2339999
Bishwo Bhandari <bhanbish@isu.edu>
2021-05-14 10:53:59 -0500

    updated results table

--- frontend/src/components/Output/ResultsTable.js
+++ frontend/src/components/Output/ResultsTable.js
@@ -30,66 +30,180 @@ class ResultsTable extends Component {
             {model_index, notes} = store.selectedOutput.selected;
 
         return (
-            <table className="table result table-sm">
-                <thead className="table-bordered">
-                    <tr className="table-primary">
-                        <th>Model</th>
-                        <th>BMDL</th>
-                        <th>BMD</th>
-                        <th>BMDU</th>
-                        <th>AIC</th>
-                        <th>p-value</th>
-                        {store.recommendationEnabled ? (
-                            <>
-                                <th>Recommendation bin</th>
-                                <th>Recommendation notes</th>
-                            </>
-                        ) : null}
-                    </tr>
-                </thead>
-                <tbody className="table-bordered">
-                    {models.map((model, idx) => {
-                        return (
-                            <tr
-                                key={idx}
-                                onMouseEnter={() => store.drPlotAddHover(model)}
-                                onMouseLeave={() => store.drPlotRemoveHover()}
-                                className={model_index == idx ? "table-success" : ""}>
-                                <td>
-                                    <a
-                                        href="#"
-                                        onClick={e => {
-                                            e.preventDefault();
-                                            store.showModalDetail(model);
-                                        }}>
-                                        {model.name}
-                                    </a>
-                                </td>
-                                <td>{ff(model.results.bmdl)}</td>
-                                <td>{ff(model.results.bmd)}</td>
-                                <td>{ff(model.results.bmdu)}</td>
-                                <td>{ff(model.results.fit.aic)}</td>
-                                <td>{ff(getPValue(dataset.dtype, model.results))}</td>
+            <div className="card mb-2">
+                <div className="card-header">Frequentist Model Results</div>
+                <div>
+                    <table className="card-table table result table-sm">
+                        <thead className="table-bordered">
+                            <tr className="table-primary">
+                                <th>Model</th>
+                                <th>BMDL</th>
+                                <th>BMD</th>
+                                <th>BMDU</th>
+                                <th>AIC</th>
+                                <th>p-value</th>
                                 {store.recommendationEnabled ? (
                                     <>
-                                        <td>{getModelBinLabel(store.selectedOutput, idx)}</td>
-                                        <td>{getRecommenderText(store.selectedOutput, idx)}</td>
+                                        <th>Recommendation bin</th>
+                                        <th>Recommendation notes</th>
                                     </>
                                 ) : null}
                             </tr>
-                        );
-                    })}
-                </tbody>
-                {notes ? (
-                    <tfoot>
-                        <tr>
-                            <td colSpan="7" className="text-muted">
-                                {notes}
-                            </td>
-                        </tr>
-                    </tfoot>
-                ) : null}
-            </table>
+                        </thead>
+                        <tbody className="table-bordered">
+                            <tr>Restricted Models</tr>
+                            {models.map((model, idx) => {
+                                return (
+                                    <tr
+                                        key={idx}
+                                        onMouseEnter={() => store.drPlotAddHover(model)}
+                                        onMouseLeave={() => store.drPlotRemoveHover()}
+                                        className={model_index == idx ? "table-success" : ""}>
+                                        <td>
+                                            <a
+                                                href="#"
+                                                onClick={e => {
+                                                    e.preventDefault();
+                                                    store.showModalDetail(model);
+                                                }}>
+                                                {model.name}
+                                            </a>
+                                        </td>
+                                        <td>{ff(model.results.bmdl)}</td>
+                                        <td>{ff(model.results.bmd)}</td>
+                                        <td>{ff(model.results.bmdu)}</td>
+                                        <td>{ff(model.results.fit.aic)}</td>
+                                        <td>{ff(getPValue(dataset.dtype, model.results))}</td>
+                                        {store.recommendationEnabled ? (
+                                            <>
+                                                <td>
+                                                    {getModelBinLabel(store.selectedOutput, idx)}
+                                                </td>
+                                                <td>
+                                                    {getRecommenderText(store.selectedOutput, idx)}
+                                                </td>
+                                            </>
+                                        ) : null}
+                                    </tr>
+                                );
+                            })}
+                            <tr>Unrestricted models</tr>
+                            {models.map((model, idx) => {
+                                return (
+                                    <tr
+                                        key={idx}
+                                        onMouseEnter={() => store.drPlotAddHover(model)}
+                                        onMouseLeave={() => store.drPlotRemoveHover()}
+                                        className={model_index == idx ? "table-success" : ""}>
+                                        <td>
+                                            <a
+                                                href="#"
+                                                onClick={e => {
+                                                    e.preventDefault();
+                                                    store.showModalDetail(model);
+                                                }}>
+                                                {model.name}
+                                            </a>
+                                        </td>
+                                        <td>{ff(model.results.bmdl)}</td>
+                                        <td>{ff(model.results.bmd)}</td>
+                                        <td>{ff(model.results.bmdu)}</td>
+                                        <td>{ff(model.results.fit.aic)}</td>
+                                        <td>{ff(getPValue(dataset.dtype, model.results))}</td>
+                                        {store.recommendationEnabled ? (
+                                            <>
+                                                <td>
+                                                    {getModelBinLabel(store.selectedOutput, idx)}
+                                                </td>
+                                                <td>
+                                                    {getRecommenderText(store.selectedOutput, idx)}
+                                                </td>
+                                            </>
+                                        ) : null}
+                                    </tr>
+                                );
+                            })}
+                        </tbody>
+                        {notes ? (
+                            <tfoot>
+                                <tr>
+                                    <td colSpan="7" className="text-muted">
+                                        {notes}
+                                    </td>
+                                </tr>
+                            </tfoot>
+                        ) : null}
+                    </table>
+                </div>
+
+                <div className="card-header">Bayesian Model Results</div>
+                <div>
+                    <table className="card-table table result table-sm">
+                        <thead className="table-bordered">
+                            <tr className="table-primary">
+                                <th>Model</th>
+                                <th>BMDL</th>
+                                <th>BMD</th>
+                                <th>BMDU</th>
+                                <th>AIC</th>
+                                <th>p-value</th>
+                                {store.recommendationEnabled ? (
+                                    <>
+                                        <th>Recommendation bin</th>
+                                        <th>Recommendation notes</th>
+                                    </>
+                                ) : null}
+                            </tr>
+                        </thead>
+                        <tbody className="table-bordered">
+                            {models.map((model, idx) => {
+                                return (
+                                    <tr
+                                        key={idx}
+                                        onMouseEnter={() => store.drPlotAddHover(model)}
+                                        onMouseLeave={() => store.drPlotRemoveHover()}
+                                        className={model_index == idx ? "table-success" : ""}>
+                                        <td>
+                                            <a
+                                                href="#"
+                                                onClick={e => {
+                                                    e.preventDefault();
+                                                    store.showModalDetail(model);
+                                                }}>
+                                                {model.name}
+                                            </a>
+                                        </td>
+                                        <td>{ff(model.results.bmdl)}</td>
+                                        <td>{ff(model.results.bmd)}</td>
+                                        <td>{ff(model.results.bmdu)}</td>
+                                        <td>{ff(model.results.fit.aic)}</td>
+                                        <td>{ff(getPValue(dataset.dtype, model.results))}</td>
+                                        {store.recommendationEnabled ? (
+                                            <>
+                                                <td>
+                                                    {getModelBinLabel(store.selectedOutput, idx)}
+                                                </td>
+                                                <td>
+                                                    {getRecommenderText(store.selectedOutput, idx)}
+                                                </td>
+                                            </>
+                                        ) : null}
+                                    </tr>
+                                );
+                            })}
+                        </tbody>
+                        {notes ? (
+                            <tfoot>
+                                <tr>
+                                    <td colSpan="7" className="text-muted">
+                                        {notes}
+                                    </td>
+                                </tr>
+                            </tfoot>
+                        ) : null}
+                    </table>
+                </div>
+            </div>
         );
     }
 }

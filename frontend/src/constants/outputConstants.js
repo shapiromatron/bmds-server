48e25a2db7ba1792c0bba9036c2649609175ccd9
Andy Shapiro <shapiromatron@gmail.com>
2021-05-18 15:53:51 -0400

    add back pvalue for continuous data

--- frontend/src/constants/outputConstants.js
+++ frontend/src/constants/outputConstants.js
@@ -1,11 +1,10 @@
 import {Dtype} from "./dataConstants";
-import {BMDS_BLANK_VALUE} from "../common";
 
 export const getPValue = function(dataType, results) {
         if (dataType === Dtype.DICHOTOMOUS) {
             return results.gof.p_value;
         } else {
-            return BMDS_BLANK_VALUE;
+            return results.tests.p_values[3];
         }
     },
     priorClass = {

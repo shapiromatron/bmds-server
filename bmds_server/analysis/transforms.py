b000cb09af2de1e2402fcbc7662ab8fc0ede914e
Andy Shapiro <shapiromatron@gmail.com>
2021-07-12 15:39:40 -0400

    use new bmds api

--- bmds_server/analysis/transforms.py
+++ bmds_server/analysis/transforms.py
@@ -5,7 +5,7 @@ import bmds
 from bmds.bmds3.sessions import get_model
 from bmds.bmds3.types.continuous import ContinuousModelSettings
 from bmds.bmds3.types.dichotomous import DichotomousModelSettings
-from bmds.bmds3.types.priors import PriorClass, get_continuous_prior, get_dichotomous_prior
+from bmds.bmds3.types.priors import PriorClass
 from bmds.constants import Dtype
 
 from .validators.datasets import AdverseDirection
@@ -38,7 +38,6 @@ def build_model_settings(
     options: Dict,
     dataset_options: Dict,
 ) -> Union[DichotomousModelSettings, ContinuousModelSettings]:
-    model = get_model(bmds_version, dataset_type, model_name)
     prior_class = bmd3_prior_map[prior_class]
     if dataset_type in bmds.constants.DICHOTOMOUS_DTYPES:
         return DichotomousModelSettings(
@@ -46,7 +45,7 @@ def build_model_settings(
             alpha=1.0 - options["confidence_level"],
             bmr_type=options["bmr_type"],
             degree=dataset_options["degree"],
-            priors=get_dichotomous_prior(model.bmd_model_class, prior_class),
+            priors=prior_class,
         )
     elif dataset_type in bmds.constants.CONTINUOUS_DTYPES:
         return ContinuousModelSettings(
@@ -57,7 +56,7 @@ def build_model_settings(
             disttype=options["dist_type"],
             degree=dataset_options["degree"],
             is_increasing=is_increasing_map[dataset_options["adverse_direction"]],
-            priors=get_continuous_prior(model.bmd_model_class, prior_class),
+            priors=prior_class,
         )
     else:
         raise ValueError(f"Unknown dataset_type: {dataset_type}")

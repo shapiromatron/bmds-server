ac48cd06d8d7f2893c8b7bae5d74113ebe8da9a8
Andy Shapiro <shapiromatron@gmail.com>
2021-07-15 14:30:50 -0400

    updates

---
+++ frontend/src/components/common/HelpTextPopover.js
@@ -0,0 +1,26 @@
+import React, {Component} from "react";
+import PropTypes from "prop-types";
+
+import Popover from "./Popover";
+
+class HelpTextPopover extends Component {
+    render() {
+        const {icon, title, content} = this.props;
+        return (
+            <Popover element={"span"} content={content} title={title}>
+                <i className={`ml-1 fa fa-fw ${icon}`}></i>
+            </Popover>
+        );
+    }
+}
+HelpTextPopover.propTypes = {
+    icon: PropTypes.string,
+    title: PropTypes.string,
+    content: PropTypes.string.isRequired,
+};
+HelpTextPopover.defaultProps = {
+    icon: "fa-question-circle",
+    title: "Help-text",
+};
+
+export default HelpTextPopover;

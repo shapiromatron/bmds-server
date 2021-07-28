edae6d3f83380ef770d125f9d503d3559269bdd2
Bishwo Bhandari <bhanbish@isu.edu>
2021-07-28 10:09:17 -0500

    updated dataset table for CI

--- frontend/src/components/Data/DatasetTable.js
+++ frontend/src/components/Data/DatasetTable.js
@@ -4,14 +4,24 @@ import {observer} from "mobx-react";
 import PropTypes from "prop-types";
 
 import {columns, columnHeaders} from "../../constants/dataConstants";
+import {Dtype} from "../../constants/dataConstants";
 
 @observer
 class DatasetTable extends Component {
     render() {
         const {dataset} = this.props,
             columnNames = columns[dataset.dtype],
-            width = `${100 / columnNames.length}%`;
-
+            width = `${100 / columnNames.length}%`,
+            dose_group = _.uniq(dataset.doses),
+            getResponseList = function(key) {
+                let response_array = [];
+                dataset.doses.map((dose, i) => {
+                    if (dose === key) {
+                        response_array.push(dataset.responses[i]);
+                    }
+                });
+                return response_array.toString().replace(/,(?=[^\s])/g, ",  ");
+            };
         return (
             <>
                 <div className="label">
@@ -33,15 +43,26 @@ class DatasetTable extends Component {
                         </tr>
                     </thead>
                     <tbody>
-                        {_.range(dataset.doses.length).map(rowIdx => {
-                            return (
-                                <tr key={rowIdx}>
-                                    {columnNames.map((column, colIdx) => {
-                                        return <td key={colIdx}>{dataset[column][rowIdx]}</td>;
-                                    })}
-                                </tr>
-                            );
-                        })}
+                        {dataset.dtype === Dtype.CONTINUOUS_INDIVIDUAL
+                            ? _.range(dose_group.length).map(rowIdx => {
+                                  return (
+                                      <tr key={rowIdx}>
+                                          <td>{dose_group[rowIdx]}</td>
+                                          <td>{getResponseList(dose_group[rowIdx])}</td>
+                                      </tr>
+                                  );
+                              })
+                            : _.range(dataset.doses.length).map(rowIdx => {
+                                  return (
+                                      <tr key={rowIdx}>
+                                          {columnNames.map((column, colIdx) => {
+                                              return (
+                                                  <td key={colIdx}>{dataset[column][rowIdx]}</td>
+                                              );
+                                          })}
+                                      </tr>
+                                  );
+                              })}
                     </tbody>
                 </table>
             </>

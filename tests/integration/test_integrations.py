fa13a8aac57ece1ba67b6f8aac464bcdddb0e5e3
Andy Shapiro <shapiromatron@gmail.com>
2021-08-10 15:27:59 -0400

    add verbose command

--- tests/integration/test_integrations.py
+++ tests/integration/test_integrations.py
@@ -5,7 +5,7 @@ import pytest
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from django.test import TestCase
 
-from .tests import dichotomous
+from .tests import continuous, dichotomous
 
 SKIP_INTEGRATION = os.environ.get("BMDS_INTEGRATION_TESTS") is None
 BROWSER = os.environ.get("BROWSER", "firefox")  # default to firefox; seems more stable
@@ -27,16 +27,15 @@ class TestIntegration(StaticLiveServerTestCase, TestCase):
     host = os.environ.get("LIVESERVER_HOST", "localhost")
     port = int(os.environ.get("LIVESERVER_PORT", 0))
 
-    # def test_continuous(self):
-    #     continuous.test_continuous(self.driver, self.live_server_url)
-    #     continuous.test_Sufficiently_Close_BMDL_4(self.driver, self.live_server_url)
-    #     continuous.test_analysisName(self.driver, self.live_server_url)
-    #     continuous.test_analysisDescription(self.driver, self.live_server_url)
-    #     continuous.test_changeBMRType(self.driver, self.live_server_url)
-    #     continuous.test_dataPath(self.driver, self.live_server_url)
-    #     continuous.test_logicPath(self.driver, self.live_server_url)
-    #     continuous.test_mainPath(self.driver, self.live_server_url)
-    #     continuous.test_ouputPath(self.driver, self.live_server_url)
+    def test_continuous(self):
+        continuous.test_continuous(self.driver, self.live_server_url)
+        continuous.test_analysisName(self.driver, self.live_server_url)
+        continuous.test_analysisDescription(self.driver, self.live_server_url)
+        continuous.test_changeBMRType(self.driver, self.live_server_url)
+        continuous.test_dataPath(self.driver, self.live_server_url)
+        continuous.test_logicPath(self.driver, self.live_server_url)
+        continuous.test_mainPath(self.driver, self.live_server_url)
+        continuous.test_ouputPath(self.driver, self.live_server_url)
 
     def test_dichotomous(self):
         dichotomous.test_dichotomous(self.driver, self.live_server_url, can_execute)

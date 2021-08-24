ec346df71c382dbd84adb09bbb0d5b391c1bccdf
bishwobhandari <bhanbish@isu.edu>
2021-08-24 12:45:19 -0500

    Dataset table update (#147)
    * update changes
    
    updated dataset fill up form
    updated bayesian model averaging checkbox
    
    * fixed issues
    
    * update on modal data
    
    * Updated model data and model options table
    
    * updated dataset table for CI
    
    * new changes
    
    * refactor and fix error
    
    Co-authored-by: Andy Shapiro <shapiromatron@gmail.com>

--- frontend/src/components/Data/DatasetTable.js
+++ frontend/src/components/Data/DatasetTable.js
@@ -4,6 +4,42 @@ import {observer} from "mobx-react";
 import PropTypes from "prop-types";
 
 import {columns, columnHeaders} from "../../constants/dataConstants";
+import {Dtype} from "../../constants/dataConstants";
+
+const dataRows = (dataset, columnNames) => {
+        return _.range(dataset.doses.length).map(rowIdx => {
+            return (
+                <tr key={rowIdx}>
+                    {columnNames.map((column, colIdx) => {
+                        return <td key={colIdx}>{dataset[column][rowIdx]}</td>;
+                    })}
+                </tr>
+            );
+        });
+    },
+    individualDataRows = dataset => {
+        const data = _.chain(_.zip(dataset.doses, dataset.responses))
+                .map(data => {
+                    return {dose: data[0], response: data[1]};
+                })
+                .value(),
+            doses = _.uniq(dataset.doses),
+            responses = doses.map(dose => {
+                return _.filter(data, resp => resp.dose === dose)
+                    .map(d => d.response.toString())
+                    .join(", ");
+            }),
+            rows = _.zip(doses, responses);
+
+        return rows.map((row, i) => {
+            return (
+                <tr key={i}>
+                    <td>{row[0]}</td>
+                    <td>{row[1]}</td>
+                </tr>
+            );
+        });
+    };
 
 @observer
 class DatasetTable extends Component {
@@ -15,7 +51,7 @@ class DatasetTable extends Component {
         return (
             <>
                 <div className="label">
-                    <label style={{marginRight: "20px"}}>Dataset Name:</label>
+                    <label>Dataset Name:&nbsp;</label>
                     {dataset.metadata.name}
                 </div>
 
@@ -27,21 +63,15 @@ class DatasetTable extends Component {
                     </colgroup>
                     <thead className="bg-custom">
                         <tr>
-                            {columnNames.map((column, i) => {
-                                return <th key={i}>{columnHeaders[column]}</th>;
-                            })}
+                            {columnNames.map((column, i) => (
+                                <th key={i}>{columnHeaders[column]}</th>
+                            ))}
                         </tr>
                     </thead>
                     <tbody>
-                        {_.range(dataset.doses.length).map(rowIdx => {
-                            return (
-                                <tr key={rowIdx}>
-                                    {columnNames.map((column, colIdx) => {
-                                        return <td key={colIdx}>{dataset[column][rowIdx]}</td>;
-                                    })}
-                                </tr>
-                            );
-                        })}
+                        {dataset.dtype === Dtype.CONTINUOUS_INDIVIDUAL
+                            ? individualDataRows(dataset)
+                            : dataRows(dataset, columnNames)}
                     </tbody>
                 </table>
             </>

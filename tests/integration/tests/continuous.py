da9df9bcc7cd9aedf3b006d0fa93adee231dcc29
Bishwo Bhandari <bhanbish@isu.edu>
2021-08-31 07:55:07 -0500

    integration test
    tests for Ci, Cs and DI

--- tests/integration/tests/continuous.py
+++ tests/integration/tests/continuous.py
@@ -6,6 +6,32 @@ def test_continuous_individual(driver, root_url, can_execute: bool):
     h.go_to(root_url)
     h.click("Create a new BMDS analysis")
 
+    # h.click(("Settings"))
+    # h.wait_until(driver.find_element_by_name("frequentist_unrestricted_Power").exists)
+    driver.find_element_by_name("frequentist_unrestricted_Power").click()
+    assert driver.find_element_by_name("frequentist_unrestricted_Power").is_selected()
+
+    h.wait_until(h.Text("Data").exists)
+    h.click(("Data"))
+
+    assert "/data" in driver.current_url
+
+    h.wait_until(h.Text("Create").exists)
+    h.click(("Create"))
+
+    h.wait_until(h.Text("Load an example dataset").exists)
+    h.click(("Load an example dataset"))
+
+    h.wait_until(h.Text("Save Analysis").exists)
+    h.click(("Save Analysis"))
+
+    h.wait_until(h.Text("Run Analysis").exists)
+    h.click(("Run Analysis"))
+
+    h.click(("Output"))
+
+    assert "/output" in driver.current_url
+
     # click main
     # update settings and select a model frequentist modal and a bayesian model (power)
     # create an option set
@@ -34,13 +60,36 @@ def test_continuous_summary(driver, root_url, can_execute: bool):
     h.go_to(root_url)
     h.click("Create a new BMDS analysis")
 
-    # click main
-    # update settings and select a model frequentist modal and a bayesian model (power)
-    # create an option set
+    # h.click(("Settings"))
+    # driver.find_element_by_name("frequentist_unrestricted_Power").click()
+    # assert driver.find_element_by_name("frequentist_unrestricted_Power").is_selected()
 
-    # create new dataset; load default
+    h.wait_until(h.Text("Data").exists)
+    h.click(("Data"))
 
-    # click logic tab; confirm content exists
+    assert "/data" in driver.current_url
+
+    h.wait_until(h.Text("Create").exists)
+    h.click(("Create"))
+
+    h.wait_until(h.Text("Load an example dataset").exists)
+    h.click(("Load an example dataset"))
+
+    h.wait_until(h.Text("Settings").exists)
+    h.click(("Settings"))
+
+    driver.find_element_by_name("frequentist_unrestricted_Power").click()
+    assert driver.find_element_by_name("frequentist_unrestricted_Power").is_selected()
+
+    h.wait_until(h.Text("Save Analysis").exists)
+    h.click(("Save Analysis"))
+
+    h.wait_until(h.Text("Run Analysis").exists)
+    h.click(("Run Analysis"))
+
+    h.click(("Output"))
+
+    assert "/output" in driver.current_url
 
     if can_execute:
         pass
--- tests/integration/tests/dichotomous.py
+++ tests/integration/tests/dichotomous.py
@@ -1,10 +1,36 @@
 import helium as h
+from selenium.webdriver.remote.webelement import WebElement
 
 
 def test_dichotomous(driver, root_url, can_execute: bool):
     h.set_driver(driver)
     h.go_to(root_url)
     h.click("Create a new BMDS analysis")
+
+    h.click(("Settings"))
+    driver.find_element_by_name("frequentist_unrestricted_Logistic").click()
+    assert driver.find_element_by_name("frequentist_unrestricted_Logistic").is_selected()
+
+    h.wait_until(h.Text("Data").exists)
+    h.click(("Data"))
+
+    assert "/data" in driver.current_url
+
+    h.wait_until(h.Text("Create").exists)
+    h.click(("Create"))
+
+    h.wait_until(h.Text("Load an example dataset").exists)
+    h.click(("Load an example dataset"))
+
+    h.wait_until(h.Text("Save Analysis").exists)
+    h.click(("Save Analysis"))
+
+    h.wait_until(h.Text("Run Analysis").exists)
+    h.click(("Run Analysis"))
+
+    h.click(("Output"))
+
+    assert "/output" in driver.current_url
 
     # click main
     # update settings and select a model frequentist modal and a bayesian model

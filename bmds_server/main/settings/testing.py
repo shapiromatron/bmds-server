d0c8d4cd8e01369bf3c819beba1ea2b27d85bc03
Andy Shapiro <shapiromatron@gmail.com>
2021-11-02 17:23:21 -0400

    wam

--- bmds_server/main/settings/testing.py
+++ bmds_server/main/settings/testing.py
@@ -1,5 +1,6 @@
 # flake8: noqa
 
+from ..constants import AuthProvider
 from .dev import *
 
 INSTALLED_APPS = [app for app in INSTALLED_APPS if app != "debug_toolbar"]
@@ -9,3 +10,5 @@ SKIN = SkinStyle.Base
 DATABASES["default"]["NAME"] = "bmds-online-test"
 
 PASSWORD_HASHERS = ("django.contrib.auth.hashers.MD5PasswordHasher",)
+
+AUTH_PROVIDERS = {AuthProvider.django, AuthProvider.external}
--- make.bat
+++ make.bat
@@ -28,7 +28,7 @@ npm --prefix .\frontend run lint
 goto :eof
 
 :format
-black . && isort -rc -y && flake8 .
+black . && isort . && flake8 .
 npm --prefix .\frontend run format
 goto :eof
 
@@ -37,7 +37,7 @@ black . --check && flake8 .
 goto :eof
 
 :format-py
-black . && isort -rc -y && flake8 .
+black . && isort . && flake8 .
 goto :eof
 
 :lint-js

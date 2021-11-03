b1e74e7c793012648d61a1410a33e2c6e9784b06
Andy Shapiro <shapiromatron@gmail.com>
2021-11-03 12:34:35 -0400

    add test

---
+++ tests/common/test_middleware.py
@@ -0,0 +1,22 @@
+import logging
+
+import pytest
+from django.test import Client
+
+
+@pytest.mark.django_db
+class TestRequestLogMiddleware:
+    def test_logging(self, caplog):
+        # setup
+        caplog.set_level(logging.INFO)
+
+        # get logs
+        client = Client()
+        client.get("/")
+
+        # make sure we have a record and it matches expected pattern
+        matches = [rec for rec in caplog.records if rec.name == "bmdsonline.request"]
+        assert len(matches) > 0
+        message = matches[0].message
+        assert "GET / 200" in message
+        assert "user-0" in message

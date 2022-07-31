af555b504bdbd891238984307420099b4ec0186b
Andy Shapiro <shapiromatron@gmail.com>
2022-07-31 00:49:17 -0400

    table text alignment

--- frontend/src/components/IndividualModel/GoodnessFit.js
+++ frontend/src/components/IndividualModel/GoodnessFit.js
@@ -13,9 +13,9 @@ class GoodnessFit extends Component {
             dataset = store.selectedDataset,
             {dtype} = dataset;
         return (
-            <table className="table table-sm table-bordered">
+            <table className="table table-sm table-bordered text-right">
                 <thead>
-                    <tr className="bg-custom">
+                    <tr className="bg-custom text-left">
                         <th colSpan="9">Goodness of Fit</th>
                     </tr>
                     <tr>

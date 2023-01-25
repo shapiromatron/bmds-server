69eebaf7beaeb80593975f3779a82be133a21b53
Andy Shapiro <shapiromatron@gmail.com>
2023-01-25 07:35:03 -0500

    disable django-debug-toolbar

--- bmds_server/main/settings/dev.py
+++ bmds_server/main/settings/dev.py
@@ -1,8 +1,8 @@
 # flake8: noqa
 from .base import *
 
-INSTALLED_APPS += ("debug_toolbar",)
-MIDDLEWARE += ("debug_toolbar.middleware.DebugToolbarMiddleware",)
+# INSTALLED_APPS += ("debug_toolbar",)
+# MIDDLEWARE += ("debug_toolbar.middleware.DebugToolbarMiddleware",)
 INTERNAL_IPS = ("127.0.0.1",)
 
 DEBUG = True

d0dcace6816d81e94ed313267e6ec6d92976a77b
Danny Peterson <dannypeterson28@gmail.com>
2023-05-11 12:16:02 -0400

    changed duration null to undefined

--- frontend/src/components/transforms/polyk/store.js
+++ frontend/src/components/transforms/polyk/store.js
@@ -12,7 +12,7 @@ class Store {
         dataset: "",
         dose_units: "ppm",
         power: 3,
-        duration: null,
+        duration: undefined,
     };
     @observable error = null;
     @observable errorObject = null;

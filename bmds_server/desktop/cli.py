23bb101e0557272f85ada07c0e9e0e778609827b
Andy Shapiro <shapiromatron@gmail.com>
2023-05-12 20:10:42 -0400

    updates

--- bmds_server/desktop/cli.py
+++ bmds_server/desktop/cli.py
@@ -1,64 +1,127 @@
 import logging
-from contextlib import redirect_stderr, redirect_stdout
 from datetime import datetime
 from importlib.metadata import version
-from threading import Thread
+from io import StringIO
+from pathlib import Path
+from threading import Event, Thread
 from time import sleep
+from webbrowser import open_new_tab
 
+from textual import on
 from textual.app import App, ComposeResult
-from textual.containers import ScrollableContainer
-from textual.widgets import Button, Footer, Header, Markdown, TextLog
-
-from .web import MyServer
+from textual.containers import Container, ScrollableContainer
+from textual.widgets import (
+    Button,
+    Footer,
+    Header,
+    Label,
+    Markdown,
+    Static,
+    TabbedContent,
+    TabPane,
+    TextLog,
+)
 
 logger = logging.getLogger(__name__)
 
-MD_HEADER = """## Welcome to BMDS Desktop.
-Here's a bunch of header text we can add to describe what this is."""
+ROOT = Path(__file__).parent
 
 
-class TextLogHandler(logging.Handler):
-    def __init__(self, widget: TextLog):
-        super().__init__()
-        self.widget = widget
-        self.widget.write("startup")
-        logger.warning("test")
-
-    def emit(self, record):
-        self.widget.write(record)
-
-
-class DjangoApp:
-    def __init__(self, tui: "BmdsDesktop"):
-        self.is_running = False
-        self.tui = tui
-        self.app_server = MyServer()
-
-    def toggle_button(self, btn: Button):
-        btn.label = "Start" if self.is_running else "Stop"
-        self.is_running = not self.is_running
-        if self.is_running:
-            with redirect_stdout(logger.handlers[0]), redirect_stderr(log_stream):
-                self.app_server.start()
+def data_folder() -> Path:
+    home = Path.home()
+    if home.is_dir() and home.exists():
+        if home.joinpath(".local", "share").exists():
+            local_dir = home.joinpath(".local", "share")
         else:
-            logger.info("Stopping...")
-            self.app_server.stop()
-            self.app_server = MyServer()
+            local_dir = home.joinpath("AppData", "Local")
+    else:
+        raise ValueError("Unsupported platform")
+
+    path = local_dir / "bmds-desktop"
+    path.mkdir(parents=True, exist_ok=True)
+    return path
 
 
 class LogApp:
-    def __init__(self, tui: App):
-        self.tui = tui
+    def __init__(self):
+        self.stream = StringIO()
+        self.handler = logging.StreamHandler(self.stream)
+        self.widget = TextLog(id="log")
+        self.thread = Thread(target=self._run, daemon=True)
 
-    def log(self, message: str):
-        logger = self.tui.query_one("#log", expect_type=TextLog)
-        logger.write(f"{datetime.now()}: {message}")
+    def add_handler(self):
+        logger = logging.getLogger()
+        logger.addHandler(self.handler)
+        logger.setLevel(logging.INFO)
+
+    def start(self):
+        self.add_handler()
+        self.thread.start()
+
+    def _run(self):
+        while True:
+            if log_contents := self.stream.getvalue().strip():
+                self.widget.write(log_contents)
+                self.stream.seek(0)
+                self.stream.truncate()
+            sleep(1)
 
 
-def add_logs(app):
-    while True:
-        logger.warning("abc")
-        sleep(1)
+class AppThread(Thread):
+    def __init__(self, **kw):
+        super().__init__(**kw)
+        self._stop_event = Event()
+
+    def run(self):
+        while not self._stop_event.is_set():
+            logger.info(f"Thread running at {datetime.now()}")
+            sleep(1)
+        logger.info(f"Thread stopped at {datetime.now()}")
+
+    def stop(self):
+        self._stop_event.set()
+
+
+class AppRunner:
+    LABEL = {True: "Stop BMDS Desktop", False: "Start BMDS Desktop"}
+
+    def __init__(self):
+        self.started = False
+        self.widget = Button(label=self.LABEL[self.started], id="runner-button", variant="primary")
+        self.thread: AppThread
+
+    def toggle(self):
+        self.started = not self.started
+        self.widget.label = self.LABEL[self.started]
+        if self.started:
+            self.thread = AppThread(daemon=True)
+            self.thread.start()
+            open_new_tab("https://bmdsonline.epa.gov")
+        else:
+            self.thread.stop()
+            self.thread = AppThread(daemon=True)
+
+    def start(self):
+        self.thread.start()
+
+
+class BmdsTabs(Static):
+    def __init__(self, _app: "BmdsDesktop", **kw):
+        self._app = _app
+        super().__init__(**kw)
+
+    def compose(self) -> ComposeResult:
+        with TabbedContent(id="tabs"):
+            with TabPane("Application", classes="app"):
+                yield Container(
+                    self._app.runner.widget,
+                )
+                yield Container(
+                    Label(f"[b]Data folder: [/b]\n  {data_folder()}"),
+                    classes="app-box",
+                )
+            with TabPane("Logging"):
+                yield self._app.log_app.widget
 
 
 class BmdsDesktop(App):
@@ -66,33 +129,25 @@ class BmdsDesktop(App):
 
     TITLE = f"BMDS Desktop (version {version('bmds_server')})"
     BINDINGS = [("q", "quit", "Quit"), ("d", "toggle_dark", "Toggle dark mode")]
+    CSS_PATH = "app.css"
 
     def __init__(self, **kw):
-        self.django_app = DjangoApp(self)
-        self.log_app = LogApp(self)
+        self.log_app = LogApp()
+        self.runner = AppRunner()
+        self.tabs = BmdsTabs(self)
         super().__init__(**kw)
-        self.text_log_widget = TextLog(id="log")
-        self.text_log = TextLogHandler(widget=self.text_log_widget)
-        logger.addHandler(self.text_log)
-
-        logger_thread = Thread(target=add_logs, daemon=True, args=(self,))
-        logger_thread.start()
 
     def compose(self) -> ComposeResult:
         """Create child widgets for the app."""
         yield Header()
         yield ScrollableContainer(
-            Markdown(MD_HEADER),
-            Button("Start", id="start"),
-            self.text_log_widget,
+            Markdown((ROOT / "content/top.md").read_text()), self.tabs, classes="main"
         )
         yield Footer()
 
-    def on_button_pressed(self, event: Button.Pressed) -> None:
-        """Event handler called when a button is pressed."""
-        if event.button.id == "start":
-            btn = self.query_one("#start", expect_type=Button)
-            self.django_app.toggle_button(btn)
+    @on(Button.Pressed, "#runner-button")
+    def toggle_runner(self):
+        self.runner.toggle()
 
     def action_quit(self):
         """Exit the application."""
@@ -102,10 +157,8 @@ class BmdsDesktop(App):
         """An action to toggle dark mode."""
         self.dark = not self.dark
 
-    def exit(self, result=None, message=None):
-        if self.django_app and self.django_app.is_running:
-            self.django_app.app_server.stop()
-        return super().exit(result, message)
+    def on_mount(self) -> None:
+        self.log_app.start()
 
 
 def main():
